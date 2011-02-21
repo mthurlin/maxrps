5d972f3eb61198913c17ba58c39ac7649077d7a0
mthurlin <markus@esn.me>
2011-02-22 00:02:55 +0100

    Some cleanup

--- maxrps.py
+++ maxrps.py
@@ -1,4 +1,3 @@
-#!/bin/sh
 import gevent
 from gevent import monkey
 monkey.patch_all()
@@ -6,29 +5,6 @@ monkey.patch_all()
 
 import time, sys, urllib2, operator
 from optparse import OptionParser
-
-
-parser = OptionParser(usage="usage: %prog [options] url")
-parser.add_option("-t", "--time", action="store", type="int", dest="time", default=250, help="Median latency accepted in milliseconds. Default: 250 ms")
-parser.add_option("-m", "--maxtime", action="store", type="int", dest="maxtime", default=1000, help="Maximum latency accepted (anything else will be a failed request) in milliseconds. Default: 1000 ms")
-parser.add_option("-e", "--errorrate", action="store", type="int", dest="errorrate", default=5, help="Maximum accepted error rate (in percent) Default: 5")
-parser.add_option("-c", "--concurrency", action="store", type="int", dest="concurrency", default=1, help="Number of concurrent requests to start at. Default: 1")
-parser.add_option("-q", "--quiet", action="store_true", dest="quiet", help="Do not print status updates in short intervals", default=False)
-
-(options, args) = parser.parse_args()
-
-if len(args) != 1:
-    parser.print_help()
-    sys.exit(1)
-    
-url = args[0]
-
-print
-print "Benchmarking %s" % (url, )
-print "Starting at %d concurrent requests" % (options.concurrency, )
-print "Accepted median latency of %d ms (maximum accepted %d ms)" % (options.time, options.maxtime)
-print
-print
 
 
 
@@ -104,6 +80,7 @@ def getReport(rps, concurrency, requests, errors, errorRate, median, mbps):
 class MaxRps(object):
     def __init__(self, url, options):
         self.url = url
+        self.options = options
         self.maxErrorRate = options.errorrate / 100.0
         self.maxMedianLatency = options.time / 1000.0
         self.maxLatency = options.maxtime / 1000.0
@@ -129,91 +106,92 @@ class MaxRps(object):
             stableStreak = 10000
             
             while True:
-                         
-                gevent.sleep(ramptime)
-                
-                if ramptime < MAXRAMPTIME:
-                    ramptime += 0.5
+                try:
+                    gevent.sleep(ramptime)
                     
-                rampDown = False
-                rampUp = 0
-                concurrency = len(self.workers)
-                
-                errorRate = self.error / (self.success + self.error) if self.success or self.error else 0.0
-                
-                currentTime = time.time()
-                elapsed = currentTime - lastTime
-                lastTime = currentTime
-                
-                rps = self.success / elapsed
-                                
-                latencies.extend(self.latencies)
-                latencies.sort()
-                if latencies:
-                    median = latencies[len(latencies) // 2]
-                else:
-                    median = 0.0
+                    if ramptime < MAXRAMPTIME:
+                        ramptime += 0.5
+                        
+                    rampDown = False
+                    rampUp = 0
+                    concurrency = len(self.workers)
                     
-                megabit = (self.bytes * 8.0) / (1024 * 1024)
-                
-                report = getReport(rps, concurrency, self.success+self.error, self.error, errorRate, median, megabit/elapsed)
-                print
-                print "#" * (len(report) + 4)
-                print "# " + report + " #"
-                print "#" * (len(report) + 4)
-                print                                           
-                
-                
-                self.success = self.error = self.bytes = 0
-                self.latencies = []
-                
-                
-                if concurrency > 1 and errorRate > self.maxErrorRate:
-                    print "Error rate too high, ramping down"
-                    rampDown = True
-                
-                elif concurrency > 1 and median > self.maxMedianLatency:
-                    print "Median latency too high, ramping down"
-                    rampDown = True
+                    errorRate = self.error / (self.success + self.error) if self.success or self.error else 0.0
                     
-                elif rps > lastRps:
-                    if hasRpsDrop:
-                        rampUp = 1
+                    currentTime = time.time()
+                    elapsed = currentTime - lastTime
+                    lastTime = currentTime
+                    
+                    rps = self.success / elapsed
+                                    
+                    latencies.extend(self.latencies)
+                    latencies.sort()
+                    if latencies:
+                        median = latencies[len(latencies) // 2]
                     else:
-                        for ramp in ramping:
-                            if ramp > concurrency:
-                                rampUp = ramp - concurrency
-                                break
-                                
-                elif rps < lastRps:
-                    rampUp = 1
-                    hasRpsDrop = True
-    
-                if rampDown:
-                    rampDownStreak += 1
-                    stableStreak = 0
-                else:
-                    rampDownStreak = 0
-                    stableStreak += 1
+                        median = 0.0
+                        
+                    megabit = (self.bytes * 8.0) / (1024 * 1024)
                     
-                
-    
-                if rampDown and self.workers:
-                    for i in range(rampDownStreak):
-                        if self.workers:
-                            worker = self.workers.pop()
-                            worker.keepGoing = False      
-                                     
-                if stableStreak > 2:
-                    for i in range(rampUp):
-                        self.workers.add(Worker(self).start())
+                    report = getReport(rps, concurrency, self.success+self.error, self.error, errorRate, median, megabit/elapsed)
+                    print
+                    print "#" * (len(report) + 4)
+                    print "# " + report + " #"
+                    print "#" * (len(report) + 4)
+                    print                                           
                     
-                lastRps = rps
-
+                    
+                    self.success = self.error = self.bytes = 0
+                    self.latencies = []
+                    
+                    
+                    if concurrency > 1 and errorRate > self.maxErrorRate:
+                        print "Error rate too high, ramping down"
+                        rampDown = True
+                    
+                    elif concurrency > 1 and median > self.maxMedianLatency:
+                        print "Median latency too high, ramping down"
+                        rampDown = True
+                        
+                    elif rps > lastRps:
+                        if hasRpsDrop:
+                            rampUp = 1
+                        else:
+                            for ramp in ramping:
+                                if ramp > concurrency:
+                                    rampUp = ramp - concurrency
+                                    break
+                                    
+                    elif rps < lastRps:
+                        rampUp = 1
+                        hasRpsDrop = True
+        
+                    if rampDown:
+                        rampDownStreak += 1
+                        stableStreak = 0
+                    else:
+                        rampDownStreak = 0
+                        stableStreak += 1
+                        
+                    
+        
+                    if rampDown and self.workers:
+                        for i in range(rampDownStreak):
+                            if self.workers:
+                                worker = self.workers.pop()
+                                worker.keepGoing = False      
+                                         
+                    if stableStreak > 2:
+                        for i in range(rampUp):
+                            self.workers.add(Worker(self).start())
+                        
+                    lastRps = rps
+                except KeyboardInterrupt:
+                    return
             
         gevent.spawn(ramper)
         
-        for i in range(options.concurrency):
+        for i in range(self.options.concurrency):
             self.workers.add(Worker(self).start())
         
         INTERVAL = 0.5
@@ -247,13 +225,36 @@ class MaxRps(object):
                 concurrency = len(self.workers)
                 errorRate = error / (success + error) if success or error else 0.0
                 
-                if not options.quiet:
+                if not self.options.quiet:
                     print "> " + getReport(success / INTERVAL, concurrency, success+error, error, errorRate, median, megabit / INTERVAL)
                 
                             
             except KeyboardInterrupt:
                 return
 
+if __name__ == "__main__":
 
-maxrps = MaxRps(url, options)
-maxrps.run()
+    parser = OptionParser(usage="usage: %prog [options] url")
+    parser.add_option("-t", "--time", action="store", type="int", dest="time", default=250, help="Median latency accepted in milliseconds. Default: 250 ms")
+    parser.add_option("-m", "--maxtime", action="store", type="int", dest="maxtime", default=1000, help="Maximum latency accepted (anything else will be a failed request) in milliseconds. Default: 1000 ms")
+    parser.add_option("-e", "--errorrate", action="store", type="int", dest="errorrate", default=5, help="Maximum accepted error rate (in percent) Default: 5")
+    parser.add_option("-c", "--concurrency", action="store", type="int", dest="concurrency", default=1, help="Number of concurrent requests to start at. Default: 1")
+    parser.add_option("-q", "--quiet", action="store_true", dest="quiet", help="Do not print status updates in short intervals", default=False)
+    
+    (options, args) = parser.parse_args()
+    
+    if len(args) != 1:
+        parser.print_help()
+        sys.exit(1)
+        
+    url = args[0]
+    
+    print
+    print "Benchmarking %s" % (url, )
+    print "Starting at %d concurrent requests" % (options.concurrency, )
+    print "Accepted median latency of %d ms (maximum accepted %d ms)" % (options.time, options.maxtime)
+    print
+    print
+        
+    maxrps = MaxRps(url, options)
+    maxrps.run()
